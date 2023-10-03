a02e8df3f9ccc458f201bda2e0c0357fd798512a
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-10-03 14:15:58 +0000

    Updating to latest FlutterFlow output.

--- lib/home_page/home_page_widget.dart
+++ lib/home_page/home_page_widget.dart
@@ -1,10 +1,13 @@
 import '/backend/schema/structs/index.dart';
 import '/backend/supabase/supabase.dart';
+import '/flutter_flow/flutter_flow_animations.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 import 'home_page_model.dart';
@@ -17,10 +20,27 @@ class HomePageWidget extends StatefulWidget {
   _HomePageWidgetState createState() => _HomePageWidgetState();
 }
 
-class _HomePageWidgetState extends State<HomePageWidget> {
+class _HomePageWidgetState extends State<HomePageWidget>
+    with TickerProviderStateMixin {
   late HomePageModel _model;
 
   final scaffoldKey = GlobalKey<ScaffoldState>();
+
+  final animationsMap = {
+    'containerOnActionTriggerAnimation': AnimationInfo(
+      trigger: AnimationTrigger.onActionTrigger,
+      applyInitialState: true,
+      effects: [
+        MoveEffect(
+          curve: Curves.easeInOut,
+          delay: 0.ms,
+          duration: 300.ms,
+          begin: Offset(-40.0, 0.0),
+          end: Offset(0.0, 0.0),
+        ),
+      ],
+    ),
+  };
 
   @override
   void initState() {
@@ -28,6 +48,12 @@ class _HomePageWidgetState extends State<HomePageWidget> {
     _model = createModel(context, () => HomePageModel());
 
     _model.textController ??= TextEditingController();
+    setupAnimations(
+      animationsMap.values.where((anim) =>
+          anim.trigger == AnimationTrigger.onActionTrigger ||
+          !anim.applyInitialState),
+      this,
+    );
   }
 
   @override
@@ -87,6 +113,104 @@ class _HomePageWidgetState extends State<HomePageWidget> {
             child: Column(
               mainAxisSize: MainAxisSize.max,
               children: [
+                Padding(
+                  padding: EdgeInsetsDirectional.fromSTEB(0.0, 0.0, 12.0, 0.0),
+                  child: InkWell(
+                    splashColor: Colors.transparent,
+                    focusColor: Colors.transparent,
+                    hoverColor: Colors.transparent,
+                    highlightColor: Colors.transparent,
+                    onTap: () async {
+                      if ((Theme.of(context).brightness == Brightness.light) ==
+                          true) {
+                        setDarkModeSetting(context, ThemeMode.dark);
+                        if (animationsMap[
+                                'containerOnActionTriggerAnimation'] !=
+                            null) {
+                          animationsMap['containerOnActionTriggerAnimation']!
+                              .controller
+                              .forward(from: 0.0);
+                        }
+                      } else {
+                        setDarkModeSetting(context, ThemeMode.light);
+                        if (animationsMap[
+                                'containerOnActionTriggerAnimation'] !=
+                            null) {
+                          animationsMap['containerOnActionTriggerAnimation']!
+                              .controller
+                              .reverse();
+                        }
+                      }
+                    },
+                    child: Container(
+                      width: 80.0,
+                      height: 40.0,
+                      decoration: BoxDecoration(
+                        color: Color(0xFFF1F4F8),
+                        borderRadius: BorderRadius.circular(20.0),
+                        border: Border.all(
+                          color: Color(0xFFE0E3E7),
+                          width: 1.0,
+                        ),
+                      ),
+                      child: Padding(
+                        padding:
+                            EdgeInsetsDirectional.fromSTEB(2.0, 2.0, 2.0, 2.0),
+                        child: Stack(
+                          alignment: AlignmentDirectional(0.0, 0.0),
+                          children: [
+                            Align(
+                              alignment: AlignmentDirectional(-0.90, 0.00),
+                              child: Padding(
+                                padding: EdgeInsetsDirectional.fromSTEB(
+                                    6.0, 0.0, 0.0, 0.0),
+                                child: Icon(
+                                  Icons.wb_sunny_rounded,
+                                  color: Color(0xFF57636C),
+                                  size: 24.0,
+                                ),
+                              ),
+                            ),
+                            Align(
+                              alignment: AlignmentDirectional(1.00, 0.00),
+                              child: Padding(
+                                padding: EdgeInsetsDirectional.fromSTEB(
+                                    0.0, 0.0, 6.0, 0.0),
+                                child: Icon(
+                                  Icons.mode_night_rounded,
+                                  color: Color(0xFF57636C),
+                                  size: 24.0,
+                                ),
+                              ),
+                            ),
+                            Align(
+                              alignment: AlignmentDirectional(1.00, 0.00),
+                              child: Container(
+                                width: 36.0,
+                                height: 36.0,
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  boxShadow: [
+                                    BoxShadow(
+                                      blurRadius: 4.0,
+                                      color: Color(0x430B0D0F),
+                                      offset: Offset(0.0, 2.0),
+                                    )
+                                  ],
+                                  borderRadius: BorderRadius.circular(30.0),
+                                  shape: BoxShape.rectangle,
+                                ),
+                              ).animateOnActionTrigger(
+                                animationsMap[
+                                    'containerOnActionTriggerAnimation']!,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
                 Padding(
                   padding: EdgeInsetsDirectional.fromSTEB(50.0, 30.0, 0.0, 0.0),
                   child: Row(
@@ -249,7 +373,7 @@ class _HomePageWidgetState extends State<HomePageWidget> {
                                                     FlutterFlowTheme.of(context)
                                                         .secondaryBackground,
                                                 borderRadius:
-                                                    BorderRadius.circular(10.0),
+                                                    BorderRadius.circular(8.0),
                                                 border: Border.all(
                                                   width: 1.0,
                                                 ),
