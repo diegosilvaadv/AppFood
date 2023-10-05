4d46546cf90a6f5808533546d122399f786ae1bf
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-10-05 13:13:03 +0000

    Updating to latest FlutterFlow output.

--- lib/components/detalhes_produto_widget.dart
+++ lib/components/detalhes_produto_widget.dart
@@ -139,131 +139,120 @@ class _DetalhesProdutoWidgetState extends State<DetalhesProdutoWidget> {
           ),
         ),
         Expanded(
-          child: Padding(
-            padding: EdgeInsetsDirectional.fromSTEB(6.0, 6.0, 6.0, 6.0),
-            child: Row(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  width: MediaQuery.sizeOf(context).width * 1.0,
-                  height: 100.0,
-                  decoration: BoxDecoration(
-                    color: FlutterFlowTheme.of(context).secondaryBackground,
-                    borderRadius: BorderRadius.circular(8.0),
-                  ),
-                  child: Row(
-                    mainAxisSize: MainAxisSize.max,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Flexible(
-                        child: Padding(
-                          padding: EdgeInsetsDirectional.fromSTEB(
-                              6.0, 6.0, 6.0, 6.0),
-                          child: Text(
-                            formatNumber(
-                              widget.valor,
-                              formatType: FormatType.decimal,
-                              decimalType: DecimalType.periodDecimal,
-                              currency: 'R\$',
-                            ),
-                            style: FlutterFlowTheme.of(context)
-                                .titleSmall
-                                .override(
-                                  fontFamily: 'Readex Pro',
-                                  color:
-                                      FlutterFlowTheme.of(context).primaryText,
-                                ),
-                          ),
+          child: Card(
+            clipBehavior: Clip.antiAliasWithSaveLayer,
+            color: FlutterFlowTheme.of(context).secondaryBackground,
+            elevation: 4.0,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8.0),
+            ),
+            child: Padding(
+              padding: EdgeInsetsDirectional.fromSTEB(6.0, 6.0, 6.0, 6.0),
+              child: Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Flexible(
+                    child: Padding(
+                      padding:
+                          EdgeInsetsDirectional.fromSTEB(6.0, 6.0, 6.0, 6.0),
+                      child: Text(
+                        formatNumber(
+                          widget.valor,
+                          formatType: FormatType.decimal,
+                          decimalType: DecimalType.periodDecimal,
+                          currency: 'R\$',
                         ),
-                      ),
-                      Flexible(
-                        child: Padding(
-                          padding: EdgeInsetsDirectional.fromSTEB(
-                              6.0, 6.0, 6.0, 6.0),
-                          child: Container(
-                            width: 160.0,
-                            height: 50.0,
-                            decoration: BoxDecoration(
-                              color: FlutterFlowTheme.of(context)
-                                  .secondaryBackground,
-                              borderRadius: BorderRadius.circular(8.0),
-                              shape: BoxShape.rectangle,
-                              border: Border.all(
-                                color: FlutterFlowTheme.of(context).alternate,
-                                width: 2.0,
-                              ),
-                            ),
-                            child: FlutterFlowCountController(
-                              decrementIconBuilder: (enabled) => FaIcon(
-                                FontAwesomeIcons.minus,
-                                color: enabled
-                                    ? FlutterFlowTheme.of(context).secondaryText
-                                    : FlutterFlowTheme.of(context).alternate,
-                                size: 20.0,
-                              ),
-                              incrementIconBuilder: (enabled) => FaIcon(
-                                FontAwesomeIcons.plus,
-                                color: enabled
-                                    ? FlutterFlowTheme.of(context).primary
-                                    : FlutterFlowTheme.of(context).alternate,
-                                size: 20.0,
-                              ),
-                              countBuilder: (count) => Text(
-                                count.toString(),
-                                style: FlutterFlowTheme.of(context).titleLarge,
-                              ),
-                              count: _model.countControllerValue ??= 1,
-                              updateCount: (count) => setState(
-                                  () => _model.countControllerValue = count),
-                              stepSize: 1,
-                              minimum: 1,
-                              maximum: 100,
-                            ),
-                          ),
-                        ),
-                      ),
-                      Flexible(
-                        child: Padding(
-                          padding: EdgeInsetsDirectional.fromSTEB(
-                              6.0, 6.0, 6.0, 6.0),
-                          child: FlutterFlowIconButton(
-                            borderColor: Color(0xFF0D820A),
-                            borderRadius: 20.0,
-                            borderWidth: 1.0,
-                            buttonSize: 40.0,
-                            fillColor: Color(0xFF13DC0C),
-                            icon: Icon(
-                              Icons.add,
+                        style: FlutterFlowTheme.of(context).titleSmall.override(
+                              fontFamily: 'Readex Pro',
                               color: FlutterFlowTheme.of(context).primaryText,
-                              size: 24.0,
                             ),
-                            onPressed: () async {
-                              setState(() {
-                                FFAppState().addToPedido(PedidoStruct(
-                                  nome: widget.nome,
-                                  preco: widget.valor,
-                                  quantidade:
-                                      _model.countControllerValue?.toDouble(),
-                                  img: widget.img,
-                                ));
-                                FFAppState().cardNumero =
-                                    FFAppState().cardNumero + 1.0;
-                                FFAppState().Soma = FFAppState().Soma +
-                                    functions.newCustomFunction(
-                                        widget.valor!,
-                                        _model.countControllerValue!
-                                            .toDouble());
-                              });
-                              context.safePop();
-                            },
+                      ),
+                    ),
+                  ),
+                  Flexible(
+                    child: Padding(
+                      padding:
+                          EdgeInsetsDirectional.fromSTEB(6.0, 6.0, 6.0, 6.0),
+                      child: Container(
+                        width: 160.0,
+                        height: 50.0,
+                        decoration: BoxDecoration(
+                          color:
+                              FlutterFlowTheme.of(context).secondaryBackground,
+                          borderRadius: BorderRadius.circular(8.0),
+                          shape: BoxShape.rectangle,
+                          border: Border.all(
+                            color: FlutterFlowTheme.of(context).alternate,
+                            width: 2.0,
                           ),
                         ),
+                        child: FlutterFlowCountController(
+                          decrementIconBuilder: (enabled) => FaIcon(
+                            FontAwesomeIcons.minus,
+                            color: enabled
+                                ? FlutterFlowTheme.of(context).secondaryText
+                                : FlutterFlowTheme.of(context).alternate,
+                            size: 20.0,
+                          ),
+                          incrementIconBuilder: (enabled) => FaIcon(
+                            FontAwesomeIcons.plus,
+                            color: enabled
+                                ? FlutterFlowTheme.of(context).primary
+                                : FlutterFlowTheme.of(context).alternate,
+                            size: 20.0,
+                          ),
+                          countBuilder: (count) => Text(
+                            count.toString(),
+                            style: FlutterFlowTheme.of(context).titleLarge,
+                          ),
+                          count: _model.countControllerValue ??= 1,
+                          updateCount: (count) => setState(
+                              () => _model.countControllerValue = count),
+                          stepSize: 1,
+                          minimum: 1,
+                          maximum: 100,
+                        ),
                       ),
-                    ],
+                    ),
                   ),
-                ),
-              ],
+                  Flexible(
+                    child: Padding(
+                      padding:
+                          EdgeInsetsDirectional.fromSTEB(6.0, 6.0, 6.0, 6.0),
+                      child: FlutterFlowIconButton(
+                        borderColor: Color(0xFF0D820A),
+                        borderRadius: 20.0,
+                        borderWidth: 1.0,
+                        buttonSize: 40.0,
+                        fillColor: Color(0xFF13DC0C),
+                        icon: Icon(
+                          Icons.add,
+                          color: FlutterFlowTheme.of(context).primaryText,
+                          size: 24.0,
+                        ),
+                        onPressed: () async {
+                          setState(() {
+                            FFAppState().addToPedido(PedidoStruct(
+                              nome: widget.nome,
+                              preco: widget.valor,
+                              quantidade:
+                                  _model.countControllerValue?.toDouble(),
+                              img: widget.img,
+                            ));
+                            FFAppState().cardNumero =
+                                FFAppState().cardNumero + 1.0;
+                            FFAppState().Soma = FFAppState().Soma +
+                                functions.newCustomFunction(widget.valor!,
+                                    _model.countControllerValue!.toDouble());
+                          });
+                          context.safePop();
+                        },
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
