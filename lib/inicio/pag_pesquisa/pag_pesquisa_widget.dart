6f356f00e9b3a1621022af046c7363d1a7602f62
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-10-06 16:24:38 +0000

    Updating to latest FlutterFlow output.

--- lib/inicio/pag_pesquisa/pag_pesquisa_widget.dart
+++ lib/inicio/pag_pesquisa/pag_pesquisa_widget.dart
@@ -1,7 +1,10 @@
+import '/backend/supabase/supabase.dart';
+import '/componentes/detalhes_produto/detalhes_produto_widget.dart';
 import '/flutter_flow/flutter_flow_icon_button.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
@@ -70,7 +73,7 @@ class _PagPesquisaWidgetState extends State<PagPesquisaWidget> {
             },
           ),
           title: Text(
-            'Search patients',
+            'Pesquisa',
             style: FlutterFlowTheme.of(context).headlineSmall,
           ),
           actions: [],
@@ -89,7 +92,7 @@ class _PagPesquisaWidgetState extends State<PagPesquisaWidget> {
                   controller: _model.textController,
                   obscureText: false,
                   decoration: InputDecoration(
-                    labelText: 'Search for patients...',
+                    labelText: 'Pesquisar',
                     labelStyle: FlutterFlowTheme.of(context).bodySmall,
                     enabledBorder: OutlineInputBorder(
                       borderSide: BorderSide(
@@ -130,6 +133,228 @@ class _PagPesquisaWidgetState extends State<PagPesquisaWidget> {
                   maxLines: null,
                   validator:
                       _model.textControllerValidator.asValidator(context),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsetsDirectional.fromSTEB(16.0, 10.0, 0.0, 0.0),
+                child: FutureBuilder<List<ProdutosRow>>(
+                  future: ProdutosTable().queryRows(
+                    queryFn: (q) => q.order('nome_produto'),
+                  ),
+                  builder: (context, snapshot) {
+                    // Customize what your widget looks like when it's loading.
+                    if (!snapshot.hasData) {
+                      return Center(
+                        child: SizedBox(
+                          width: 50.0,
+                          height: 50.0,
+                          child: CircularProgressIndicator(
+                            valueColor: AlwaysStoppedAnimation<Color>(
+                              FlutterFlowTheme.of(context).primary,
+                            ),
+                          ),
+                        ),
+                      );
+                    }
+                    List<ProdutosRow> listaComidaProdutosRowList =
+                        snapshot.data!;
+                    return Column(
+                      mainAxisSize: MainAxisSize.max,
+                      children: List.generate(listaComidaProdutosRowList.length,
+                          (listaComidaIndex) {
+                        final listaComidaProdutosRow =
+                            listaComidaProdutosRowList[listaComidaIndex];
+                        return Padding(
+                          padding: EdgeInsetsDirectional.fromSTEB(
+                              0.0, 6.0, 0.0, 0.0),
+                          child: InkWell(
+                            splashColor: Colors.transparent,
+                            focusColor: Colors.transparent,
+                            hoverColor: Colors.transparent,
+                            highlightColor: Colors.transparent,
+                            onTap: () async {
+                              await showModalBottomSheet(
+                                isScrollControlled: true,
+                                backgroundColor: Color(0x00F1F4F8),
+                                barrierColor: Color(0x8A000000),
+                                enableDrag: false,
+                                context: context,
+                                builder: (context) {
+                                  return GestureDetector(
+                                    onTap: () => _model
+                                            .unfocusNode.canRequestFocus
+                                        ? FocusScope.of(context)
+                                            .requestFocus(_model.unfocusNode)
+                                        : FocusScope.of(context).unfocus(),
+                                    child: Padding(
+                                      padding: MediaQuery.viewInsetsOf(context),
+                                      child: Container(
+                                        height: 550.0,
+                                        child: DetalhesProdutoWidget(
+                                          nome: listaComidaProdutosRow
+                                              .nomeProduto!,
+                                          descricao:
+                                              listaComidaProdutosRow.descricao!,
+                                          img: listaComidaProdutosRow.img!,
+                                          valor: listaComidaProdutosRow
+                                              .valorPromo!,
+                                          id: listaComidaProdutosRow.id
+                                              .toString(),
+                                          data:
+                                              listaComidaProdutosRow.createdAt,
+                                        ),
+                                      ),
+                                    ),
+                                  );
+                                },
+                              ).then((value) => safeSetState(() {}));
+                            },
+                            child: Card(
+                              clipBehavior: Clip.antiAliasWithSaveLayer,
+                              color: FlutterFlowTheme.of(context)
+                                  .secondaryBackground,
+                              elevation: 4.0,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(8.0),
+                              ),
+                              child: Padding(
+                                padding: EdgeInsetsDirectional.fromSTEB(
+                                    10.0, 10.0, 10.0, 10.0),
+                                child: Row(
+                                  mainAxisSize: MainAxisSize.max,
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Column(
+                                      mainAxisSize: MainAxisSize.max,
+                                      children: [
+                                        ClipRRect(
+                                          borderRadius:
+                                              BorderRadius.circular(8.0),
+                                          child: CachedNetworkImage(
+                                            fadeInDuration:
+                                                Duration(milliseconds: 500),
+                                            fadeOutDuration:
+                                                Duration(milliseconds: 500),
+                                            imageUrl: valueOrDefault<String>(
+                                              listaComidaProdutosRow.img,
+                                              'https://cdn.deliway.com.br/blog/base/e27/c04/53b/bife-parmegiana.jpg',
+                                            ),
+                                            width: 100.0,
+                                            height: 100.0,
+                                            fit: BoxFit.cover,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                    Expanded(
+                                      child: Padding(
+                                        padding: EdgeInsetsDirectional.fromSTEB(
+                                            6.0, 6.0, 6.0, 6.0),
+                                        child: Column(
+                                          mainAxisSize: MainAxisSize.max,
+                                          children: [
+                                            Row(
+                                              mainAxisSize: MainAxisSize.max,
+                                              children: [
+                                                Expanded(
+                                                  child: Text(
+                                                    listaComidaProdutosRow
+                                                        .nomeProduto!,
+                                                    style: FlutterFlowTheme.of(
+                                                            context)
+                                                        .labelLarge
+                                                        .override(
+                                                          fontFamily:
+                                                              'Readex Pro',
+                                                          color: FlutterFlowTheme
+                                                                  .of(context)
+                                                              .primaryText,
+                                                          fontSize: 18.0,
+                                                        ),
+                                                  ),
+                                                ),
+                                              ],
+                                            ),
+                                            Row(
+                                              mainAxisSize: MainAxisSize.max,
+                                              children: [
+                                                Expanded(
+                                                  child: Text(
+                                                    listaComidaProdutosRow
+                                                        .descricao!
+                                                        .maybeHandleOverflow(
+                                                      maxChars: 40,
+                                                      replacement: '…',
+                                                    ),
+                                                    style: FlutterFlowTheme.of(
+                                                            context)
+                                                        .bodyMedium,
+                                                  ),
+                                                ),
+                                              ],
+                                            ),
+                                            Row(
+                                              mainAxisSize: MainAxisSize.max,
+                                              children: [
+                                                Expanded(
+                                                  child: Text(
+                                                    listaComidaProdutosRow.id
+                                                        .toString()
+                                                        .maybeHandleOverflow(
+                                                          maxChars: 40,
+                                                          replacement: '…',
+                                                        ),
+                                                    style: FlutterFlowTheme.of(
+                                                            context)
+                                                        .bodyMedium,
+                                                  ),
+                                                ),
+                                              ],
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                    ),
+                                    Column(
+                                      mainAxisSize: MainAxisSize.max,
+                                      children: [
+                                        Text(
+                                          'R\$',
+                                          style: FlutterFlowTheme.of(context)
+                                              .titleLarge
+                                              .override(
+                                                fontFamily: 'Outfit',
+                                                color: Color(0xFF0D820A),
+                                                fontWeight: FontWeight.bold,
+                                              ),
+                                        ),
+                                        Text(
+                                          formatNumber(
+                                            listaComidaProdutosRow.valorPromo!,
+                                            formatType: FormatType.decimal,
+                                            decimalType:
+                                                DecimalType.periodDecimal,
+                                          ),
+                                          style: FlutterFlowTheme.of(context)
+                                              .bodyMedium
+                                              .override(
+                                                fontFamily: 'Readex Pro',
+                                                color: Color(0xFF0D820A),
+                                                fontSize: 20.0,
+                                              ),
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                        );
+                      }),
+                    );
+                  },
                 ),
               ),
             ],
